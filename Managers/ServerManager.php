76d1753dbd391aad1e6b54bf55fd8a69b8692795
GothShoot <romuald59161@gmail.com>
2019-08-09 16:23:12 +0200

    Create ServerManager.php

---
+++ Managers/ServerManager.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PlumeSolution\Server\Managers;
+
+use React\EventLoop\LoopInterface;
+use React\Http\Server;
+use React\Socket\Server as Socket;
+use Thunder\Application;
+
+class ServerManager
+{
+	private $params;
+
+	public function __construct(array $params)
+	{
+		$this->params = $params;
+	}
+
+	public function createServer(Application $application, LoopInterface $loop)
+	{
+		$server = new Server($this->generateCallback($application));
+		$socket = new Socket($this->params['port'], $loop);
+		$server->listen($socket);
+		echo 'System Online '.$this->params['url'].':'.$this->params['port'].'\n';
+	}
+
+	private function generateCallback(Application $application)
+	{
+		return function(Psr\Http\Message\ServerRequestInterface $request) use ($application)
+		{
+			try
+			{
+				$response = $application->handle($request);
+			}
+			catch (\Throwable $e)
+			{
+				return new React\Http\Response(
+					500,
+					['Content-Type' => 'text/plain'],
+					$e->getMessage()
+				);
+			}
+			return $response;
+		};
+	}
+}
