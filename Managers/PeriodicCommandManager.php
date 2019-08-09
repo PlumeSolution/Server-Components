f3ffec8e1a48c193663ee30a1299fda9dcc36894
GothShoot <romuald59161@gmail.com>
2019-08-09 16:23:10 +0200

    Create PeriodicCommandManager.php

---
+++ Managers/PeriodicCommandManager.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace PlumeSolution\Server\Managers;
+
+
+use React\EventLoop\LoopInterface;
+use Thunder\Application;
+
+class PeriodicCommandManager
+{
+	private $params;
+
+	public function __construct(array $params)
+	{
+		$this->params = $params;
+	}
+
+	public function createTimers(Application $application, LoopInterface $loop)
+	{
+		foreach ($this->params as $param)
+		{
+			$loop->addPeriodicTimer($param['timer'],
+				function ($param) use ($application)
+				{
+					$application = new Console($application);
+					$application->run($param['input']);
+				}
+			);
+		}
+	}
+}
